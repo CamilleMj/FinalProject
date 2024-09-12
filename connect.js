55d0e8d3c4c4ffb752df0f288e285ff01920b753
CamilleMj <camilleB512@hotmail.com>
2024-09-12 19:38:16 +0400

    DI Courses upload

--- connect.js
+++ connect.js
@@ -51,8 +51,9 @@ const client = new Client({
   host: process.env.PG_HOST,
   port: process.env.PG_PORT,
   user: process.env.PG_USER,
+  database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
-  database: process.env.DATABASE_URL,
+  databaseurl: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
